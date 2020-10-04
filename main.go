ae6127d953c3d82776d7badd10a657d7d56d6b4d
andre_puankare <baybaraandrey@gmail.com>
2020-10-04 18:58:56 +0300

    add welcome

--- main.go
+++ main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	_"fmt"
-	"os"
+	"fmt"
 	"github.com/baybaraandrey/elephant/repl"
 	"github.com/fatih/color"
+	"os"
 )
 
 const (
-	elephant = `
-___________.__                .__                   __   
+	elephant = `___________.__                .__                   __   
 \_   _____/|  |   ____ ______ |  |__ _____    _____/  |_ 
  |    __)_ |  | _/ __ \\____ \|  |  \\__  \  /    \   __\
  |        \|  |_\  ___/|  |_> >   Y  \/ __ \|   |  \  |  
 /_______  /|____/\___  >   __/|___|  (____  /___|  /__|  
         \/           \/|__|        \/     \/     \/      
 `
-	version = "v0.0.1" 
+	version = "v0.0.1"
 )
 
 func main() {
-	color.Blue("%s%s", elephant, version)
+	welcome()
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func welcome() {
+	color.Blue("%s%s", elephant, version)
+	fmt.Println()
 }
