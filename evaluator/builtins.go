8280ac5314f830cde70b1176b510a1ff4b6a3c25
baybaraandrey <baybaraandrey@gmail.com>
2022-06-27 14:26:12 +0300

    add builtin function exit

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,8 @@
 package evaluator
 
 import (
+	"os"
+
 	"github.com/baybaraandrey/elephant/object"
 )
 
@@ -15,11 +17,53 @@ var builtins = map[string]*object.Builtin{
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `len` not supported, got %s",
 					args[0].Type())
 			}
 
+		},
+	},
+	"first": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to 'first' must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return NULL
+		},
+	},
+	"exit": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			switch len(args) {
+			case 0:
+				os.Exit(0)
+			case 1:
+				if args[0].Type() != object.INTEGER_OBJ {
+					return newError("argument to 'exit' must be INT, got %s",
+						args[0].Type())
+
+				}
+				status := args[0].(*object.Integer).Value
+				os.Exit(int(status))
+			default:
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			return NULL
 		},
 	},
 }
