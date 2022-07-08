63e1119fc82d5d2b0f6ea16838d785ee7b8da2c3
baybaraandrey <baybaraandrey@gmail.com>
2022-07-08 16:40:21 +0300

    emit OpAdd

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,8 @@
 package compiler
 
 import (
+	"fmt"
+
 	"github.com/baybaraandrey/elephant/ast"
 	"github.com/baybaraandrey/elephant/code"
 	"github.com/baybaraandrey/elephant/object"
@@ -41,6 +43,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 		err = c.Compile(node.Right)
 		if err != nil {
 			return err
+		}
+
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
